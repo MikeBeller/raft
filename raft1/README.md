c1920c6b00a2748bb6a088fd3e6b197eff645a7c
Mike Beller <mike@beller.tech>
2021-04-13 11:02:37 -0400

    gitignore

---
+++ raft1/README.md
@@ -0,0 +1,3 @@
+# Raft
+
+Phase 1 -- put the whole algorithm in a single server.ex based on GenStateMachine
---
+++ raft1/mix.exs
@@ -0,0 +1,28 @@
+defmodule Raft.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :raft,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Raft.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:gen_state_machine, "~> 3.0"},
+    ]
+  end
+end
