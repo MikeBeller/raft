f788564484555145d578434fd9bb889ca8eaffc2
Mike Beller <mike@beller.tech>
2021-04-17 17:56:41 -0400

    Test election requests/responses in follower state

--- raft1/lib/raft/consensus.ex
+++ raft1/lib/raft/consensus.ex
@@ -14,7 +14,7 @@ defmodule Raft.Consensus do
     @type time :: Raft.time
     @type log_entry :: {non_neg_integer(), non_neg_integer(), term()}
 
-    defstruct [:term, :voted_for, :responses, :log, :me, :nodes, :commit_index, :last_applied, :next_index, :match_index, :last_event_time]
+    defstruct [:term, :voted_for, :responses, :log, :me, :nodes, :leader, :commit_index, :last_applied, :next_index, :match_index, :last_event_time]
 
     @type t :: %__MODULE__{
       term: non_neg_integer(),
@@ -23,6 +23,7 @@ defmodule Raft.Consensus do
       log: list(log_entry()),
       me: nil | addr(),
       nodes: list(addr),
+      leader: nil | addr(),
       commit_index: non_neg_integer(),
       last_applied: non_neg_integer(),
       next_index: %{addr() => non_neg_integer()},
@@ -31,7 +32,7 @@ defmodule Raft.Consensus do
     }
 
     def new() do
-      %__MODULE__{term: 0, voted_for: nil, responses: 0, log: [], me: nil, nodes: [], commit_index: 0, last_applied: 0, next_index: %{}, match_index: %{}, last_event_time: 0}
+      %__MODULE__{term: 0, voted_for: nil, responses: 0, log: [], me: nil, nodes: [], leader: nil, commit_index: 0, last_applied: 0, next_index: %{}, match_index: %{}, last_event_time: 0}
     end
   end
 
@@ -40,7 +41,7 @@ defmodule Raft.Consensus do
   @type send_action() :: {:send, list(addr()), RPC.t}
   @type action() :: set_timer_action() | cancel_timer_action() | send_action()
 
-  @type result() :: {atom(), Data.t, list(action())}
+  @type event_result() :: {atom(), Data.t, list(action())}
 
   @spec action_set_timer(atom(), non_neg_integer()) :: set_timer_action()
   defp action_set_timer(name, timeout), do: {:set_timer, name, timeout}
@@ -64,11 +65,23 @@ defmodule Raft.Consensus do
     min - 1 + :rand.uniform(max - min + 1)
   end
 
-  @spec quorum(%Data{}, %{addr() => boolean()}) :: boolean()
-  def quorum(%Data{nodes: nodes}, responses) do
-    nn = map_size(nodes) + 1
+  @spec quorum?(%Data{}, %{addr() => boolean()}) :: boolean()
+  def quorum?(%Data{nodes: nodes}, responses) do
+    nn = length(nodes) + 1
     nv = Enum.count(responses, fn {_k, v} -> v end)
     nv > nn / 2
+  end
+
+  defp candidate_log_up_to_date?(%RPC.RequestVoteReq{} = req, %Data{} = data) do
+    lli = last_log_index(data)
+    llt = last_log_term(data)
+    cond do
+      req.term > llt -> true
+      req.term < llt -> false
+      req.term == llt and req.last_log_index > lli -> true
+      req.term == llt and req.last_log_index < lli -> false
+      true -> true
+    end
   end
 
   @spec election_timeout() :: non_neg_integer()
@@ -79,7 +92,7 @@ defmodule Raft.Consensus do
     raise "oops"
   end
 
-  @spec init(addr()) :: {atom(), Data.t, list(action)}
+  @spec init(addr()) :: event_result()
   def init(me) do
     data = %{Data.new() | me: me}
     {:init, data, []}
@@ -90,6 +103,20 @@ defmodule Raft.Consensus do
   @type message_event() :: {:recv, RPC.t}
   @type event() :: config_event() | timeout_event() | message_event()
   @spec ev(atom(), event(), Data.t) :: {atom(), Data.t, list(action())}
+
+  ## State transition functions
+  
+  @spec become_candidate(Data.t) :: event_result()
+  def become_candidate(%Data{} = data) do
+    term = data.term + 1
+    {:candidate, %{data | term: term, voted_for: data.me, responses: %{data.me => true}},
+      [
+        action_set_timer(:election, election_timeout()),
+        action_send(data.nodes, %RPC.RequestVoteReq{term: term, from: data.me,
+            last_log_index: last_log_index(data), last_log_term: last_log_term(data)}),
+      ]
+    }
+  end
 
   ## INIT
   #
@@ -105,28 +132,33 @@ defmodule Raft.Consensus do
   #
 
   # Election timeout
-  def ev(:follower, {:timeout, :election}, %Data{term: term, me: me, nodes: nodes} = data) do
-    term = term + 1
-    {:candidate, %{data | term: term, voted_for: me, responses: %{me => true}},
-      [
-        action_set_timer(:election, election_timeout()),
-        action_send(nodes, %RPC.RequestVoteReq{term: term, from: me,
-            last_log_index: last_log_index(data), last_log_term: last_log_term(data)}),
-      ]
-    }
+  def ev(:follower, {:timeout, :election}, %Data{} = data) do
+    become_candidate(data)
   end
 
   # RequestVote received
-  def ev(:follower, {:recv, %RPC.RequestVoteReq{}} = req, %Data{} = data) do
-    raise "unimplemented"
+  def ev(:follower, {:recv, %RPC.RequestVoteReq{} = req}, %Data{} = data) do
+    if req.term >= data.term
+      and (data.voted_for == nil or req.from == data.voted_for)
+      and candidate_log_up_to_date?(req, data) do
+      data = %{data | term: req.term, voted_for: req.from}
+      {:follower, data,
+        [
+          action_send([req.from], %RPC.RequestVoteResp{from: data.me, term: req.term, granted: true}),
+          reset_election_timer(),
+        ]}
+    else
+      {:follower, data,
+        [action_send([req.from], %RPC.RequestVoteResp{from: data.me, term: data.term, granted: false})]}
+    end
   end
 
   ## CANDIDATE
   #
-  # RequestVote responses:
+  # RequestVote responses received:
   #
-  # all negative vote responses
-  def ev(:candidate, {:recv, %RPC.RequestVoteResp{} = resp}, %Data{} = data) do
+  # all negative vote responses received
+  def ev(:candidate, {:recv, %RPC.RequestVoteResp{granted: false} = resp}, %Data{} = data) do
     cond do 
       resp.term > data.term ->
         {:follower, %{data | term: resp.term, responses: %{}, leader: nil}, [reset_election_timer()]}
@@ -137,10 +169,10 @@ defmodule Raft.Consensus do
     end
   end
 
-  # all positive vote responses
-  def ev(:candidate, {:recv, %RPC.RequestVoteResp{} = resp}, %Data{} = data) do
+  # all positive vote responses received
+  def ev(:candidate, {:recv, %RPC.RequestVoteResp{granted: true} = resp}, %Data{} = data) do
     responses = Map.put(data.responses, resp.from, true)
-    case quorum(data, responses) do
+    case quorum?(data, responses) do
       true ->
         #become_leader(data)
         raise "unimplemented"
--- raft1/test/raft_consensus_test.exs
+++ raft1/test/raft_consensus_test.exs
@@ -3,9 +3,28 @@ defmodule Raft.ConsensusTest do
   alias Raft.Consensus
   alias Raft.RPC
 
+  defp newdata() do
+    %Consensus.Data{me: :a, term: 0, commit_index: 0, last_applied: 0, log: []}
+  end
+
   test "random range" do
     all = for _ <- 1..1000, into: MapSet.new(), do: Consensus.random_range(2,13)
     assert all == MapSet.new(2..13)
+  end
+
+  test "quorum?" do
+    data = newdata()
+    data = %{data | me: :a, nodes: [:b, :c]}
+    assert Consensus.quorum?(data, %{:a => true, :b => true})
+    assert Consensus.quorum?(data, %{:a => true, :c => true})
+    refute Consensus.quorum?(data, %{:a => true})
+
+    data = newdata()
+    data = %{data | me: :a, nodes: [:b, :c, :d, :e]}
+    assert Consensus.quorum?(data, %{:a => true, :b => true, :c => true})
+    assert Consensus.quorum?(data, %{:a => true, :c => true, :e => true})
+    refute Consensus.quorum?(data, %{:a => true, :b => true})
+    refute Consensus.quorum?(data, %{:a => true, :d => true})
   end
 
   test "init waits for nodes" do
@@ -19,6 +38,39 @@ defmodule Raft.ConsensusTest do
     assert MapSet.new(new_data.nodes) == MapSet.new([:b, :c])
     assert [{:set_timer, :election, n}] = actions
     assert n >= 150 and n <= 300
+  end
+
+  test "vote request as follower" do
+    {:init, data, []} = Consensus.init(:a)
+    {:follower, base_data, _actions} = Consensus.ev(:init, {:config, [:a, :b, :c]}, data)
+    base_req = %RPC.RequestVoteReq{from: :b, term: 1, last_log_index: 0, last_log_term: 0}
+    # test a set of voting scenarios in the follower state
+    [
+      # initial election, accept
+      {[term: 0, voted_for: nil, log: []], [term: 1, from: :b, last_log_index: 0, last_log_term: 0], {1, true}},
+      # requestor term is lower than mine, reject
+      {[term: 7, voted_for: nil, log: []], [term: 6, from: :b, last_log_index: 0, last_log_term: 0], {7, false}},
+      # I already voted, reject
+      {[term: 0, voted_for: :c, log: []], [term: 1, from: :b, last_log_index: 0, last_log_term: 0], {0, false}},
+      # Requestor's logs are up to date, accept
+      {[term: 1, voted_for: nil, log: [{1, 1, :foo}]], [term: 2, from: :b, last_log_index: 1, last_log_term: 1], {2, true}},
+      # Requestor's logs are not up to date, reject
+      {[term: 1, voted_for: nil, log: [{1, 1, :foo}]], [term: 2, from: :b, last_log_index: 0, last_log_term: 1], {2, true}},
+      {[term: 1, voted_for: nil, log: [{1, 1, :foo}]], [term: 2, from: :b, last_log_index: 1, last_log_term: 0], {2, true}},
+    ]
+    |> Enum.each(fn {dt, rq, {rsTerm, granted}} ->
+      data = struct!(base_data, dt)
+      req = struct!(base_req, rq)
+      assert {:follower, new_data, actions} = Consensus.ev(:follower, {:recv, req}, data)
+      assert [sendcmd | rest] = actions
+      assert {:send, [:b], %RPC.RequestVoteResp{from: :a, term: ^rsTerm, granted: ^granted}} = sendcmd
+      if granted do
+        assert [{:set_timer, :election, _}] = rest
+        assert new_data.term == req.term and new_data.voted_for == req.from
+      else
+        assert new_data == data
+      end
+    end)
   end
 
   test "follower election timeout" do
